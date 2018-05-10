8fab020283b653559f701a6cd54f710d13f8183d
周祥 <200592114@qq.com>
2018-05-10 15:57:47 +0800

    调整

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");  
-const webpack = require("webpack");  
-const uglify = require("uglifyjs-webpack-plugin");  
+const webpack = require("webpack"); 
   
 module.exports = {  
     devtool: 'source-map',  
